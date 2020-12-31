f571a8786921fd9eb49daabdff933c70c147f100
satyrian <51024616+satyrian@users.noreply.github.com>
2020-12-31 17:52:36 +0300

    + added slider and burger menu

---
+++ js/main.js
@@ -0,0 +1,50 @@
+$(function() {
+    let header = $("#header");
+    let intro = $("#intro");
+    let introH = intro.innerHeight();
+    let scrollPos = $(window).scrollTop();
+    let nav = $("#nav");
+    let navToggle = $("#navToggle");
+
+    checkScroll(scrollPos, introH)
+
+    $(window).on("scroll resize", function () {
+        introH = intro.innerHeight();
+        scrollPos = $(this).scrollTop();
+        checkScroll(scrollPos, introH)
+    });
+
+    function checkScroll(scrollPos, introH) {
+        if (scrollPos > introH) {
+            header.addClass("fixed");
+        } else {
+            header.removeClass("fixed");
+        }
+    }
+
+    $("[data-scroll]").on("click", function (event) {
+       event.preventDefault();
+
+       let blockId = $(this).data("scroll");
+       let blockOffset = $(blockId).offset().top;
+
+       nav.removeClass("show");
+
+       $("html, body").animate({
+          scrollTop: blockOffset - 70
+       }, 700);
+    });
+
+
+    navToggle.on("click", function (event) {
+       event.preventDefault();
+       nav.toggleClass("show");
+    });
+
+    let slider = $("#reviewsSlider");
+    slider.slick({
+        fade: true,
+        arrows: false,
+        dots: true
+    });
+});
